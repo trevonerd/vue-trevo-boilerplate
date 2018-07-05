c31042cfbb45669d6de584dd85a2aa4777d4cdc0
Marco Trevisani <marco.trevisani81@gmail.com>
2018-07-05 02:33:48 +0200

    added prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    printWidth: 120,
+    parser: 'flow',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+};
