ebb840bbeec43a135ef2e8547c33f81d43483b86
Marco Trevisani <info@djtrevo.com>
2018-07-06 02:08:27 +0200

    little refactor

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: {
     'postcss-mixins': {
-      mixins: require('postcss-mixins-css-grid').default(),
+      mixins: { 
+        ...require('postcss-mixins-css-grid').default()
+      },
     },
     'postcss-preset-env': {
       stage: 0,
